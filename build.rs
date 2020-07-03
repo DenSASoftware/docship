488b75c304c70d287b000a5568e74c73c1b7c4fd
DenSASoftware <social@densa.software>
2020-07-03 18:10:51 +0200

    use rust native zip

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
-use std::process::{Command, Stdio};
+use std::process::Command;
+use walkdir::WalkDir;
+use zip::{ZipWriter, write::FileOptions};
 
 fn main() {
     println!("cargo:rerun-if-changed=docs");
@@ -11,6 +13,14 @@ fn main() {
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
     let dest_path = std::path::Path::new(&out_dir).join("docs.zip");
 
-    let zip_result = Command::new("zip").arg("-r").arg(&dest_path).arg(".").current_dir("docs/book").status().expect("could not execute zip");
+    let files = WalkDir::new("docs/book").into_iter().collect::<Result<Vec<_>, _>>();
+    let mut archive = ZipWriter::new(std::fs::File::create(dest_path).unwrap());
+    for file in files.unwrap().into_iter().filter(|e| e.file_type().is_file()) {
+        archive.start_file_from_path(file.path().strip_prefix("docs/book").unwrap_or(file.path()), FileOptions::default().compression_method(zip::CompressionMethod::Deflated)).unwrap();
+        
+        std::io::copy(&mut std::fs::File::open(file.path()).unwrap(), &mut archive);
+    }
+
+    archive.finish().unwrap();
 }
 
