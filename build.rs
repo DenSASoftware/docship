f2de5873836da5d17cc31085fb02820111e62e56
DenSASoftware <social@densa.software>
2020-07-03 18:46:32 +0200

    format with cargo fmt

--- build.rs
+++ build.rs
@@ -1,11 +1,14 @@
 use std::process::Command;
 use walkdir::WalkDir;
-use zip::{ZipWriter, write::FileOptions};
+use zip::{write::FileOptions, ZipWriter};
 
 fn main() {
     println!("cargo:rerun-if-changed=docs/src");
 
-    let mdbook_result = Command::new("mdbook").args(&["build", "docs"]).status().expect("could not execute mdbook");
+    let mdbook_result = Command::new("mdbook")
+        .args(&["build", "docs"])
+        .status()
+        .expect("could not execute mdbook");
     if !mdbook_result.success() {
         panic!("mdbook did not run successfully");
     }
@@ -13,15 +16,27 @@ fn main() {
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
     let dest_path = std::path::Path::new(&out_dir).join("docs.zip");
 
-    let files = WalkDir::new("docs/book").into_iter().collect::<Result<Vec<_>, _>>().expect("Could not get full path list of docs/book/");
-    let mut archive = ZipWriter::new(std::fs::File::create(&dest_path).expect(&format!("Could not create file {:?}", dest_path)));
+    let files = WalkDir::new("docs/book")
+        .into_iter()
+        .collect::<Result<Vec<_>, _>>()
+        .expect("Could not get full path list of docs/book/");
+    let mut archive = ZipWriter::new(
+        std::fs::File::create(&dest_path).expect(&format!("Could not create file {:?}", dest_path)),
+    );
     for entry in files.into_iter().filter(|e| e.file_type().is_file()) {
         let file = entry.path();
-        archive.start_file_from_path(file.strip_prefix("docs/book").unwrap_or(file), FileOptions::default().compression_method(zip::CompressionMethod::Deflated)).unwrap();
-        
-        std::io::copy(&mut std::fs::File::open(file).unwrap(), &mut archive).expect(&format!("Writing {:?} to zip file failed", file));
+        archive
+            .start_file_from_path(
+                file.strip_prefix("docs/book").unwrap_or(file),
+                FileOptions::default().compression_method(zip::CompressionMethod::Deflated),
+            )
+            .unwrap();
+
+        std::io::copy(&mut std::fs::File::open(file).unwrap(), &mut archive)
+            .expect(&format!("Writing {:?} to zip file failed", file));
     }
 
-    archive.finish().expect("Could not finish zip file. So close");
+    archive
+        .finish()
+        .expect("Could not finish zip file. So close");
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use zip::read::ZipArchive;
-use std::io::{Cursor, Read};
-use tiny_http::{Response, Method, Request};
-use url::Url;
 use percent_encoding::percent_decode_str;
+use std::io::{Cursor, Read};
+use tiny_http::{Method, Request, Response};
+use url::Url;
+use zip::read::ZipArchive;
 
 const DOCS: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/docs.zip"));
 
 fn main() {
     let mut archive = ZipArchive::new(Cursor::new(DOCS)).expect("Could not open zip file");
-    let server = tiny_http::Server::http("127.0.0.1:10101").expect("Could not listen on localhost:10101");
+    let server =
+        tiny_http::Server::http("127.0.0.1:10101").expect("Could not listen on localhost:10101");
 
-    std::thread::spawn(|| opener::open("http://localhost:10101/").expect("Could not open the browser, the server is still running though"));
+    std::thread::spawn(|| {
+        opener::open("http://localhost:10101/")
+            .expect("Could not open the browser, the server is still running though")
+    });
 
     fn respond(req: Request, res: Response<impl Read>) {
         if let Err(err) = req.respond(res) {
@@ -31,19 +35,29 @@ fn main() {
 
             match archive.by_name(&path.trim_start_matches("/")) {
                 Ok(file) => {
-                    let mime_type = mime_guess::from_path(path.as_ref()).first_or(mime_guess::mime::TEXT_PLAIN);
+                    let mime_type =
+                        mime_guess::from_path(path.as_ref()).first_or(mime_guess::mime::TEXT_PLAIN);
 
-                    respond(request, tiny_http::Response::new(
-                        (200).into(),
-                        vec![tiny_http::Header::from_bytes("content-type", mime_type.as_ref()).unwrap()],
-                        file,
-                        None,
-                        None,
-                    ));
-                },
+                    respond(
+                        request,
+                        tiny_http::Response::new(
+                            (200).into(),
+                            vec![
+                                tiny_http::Header::from_bytes("content-type", mime_type.as_ref())
+                                    .unwrap(),
+                            ],
+                            file,
+                            None,
+                            None,
+                        ),
+                    );
+                }
                 Err(zip::result::ZipError::FileNotFound) => {
-                    respond(request, tiny_http::Response::from_string("").with_status_code(404));
-                },
+                    respond(
+                        request,
+                        tiny_http::Response::from_string("").with_status_code(404),
+                    );
+                }
                 _ => panic!(),
             }
         }
